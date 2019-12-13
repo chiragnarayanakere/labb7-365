ffb43deb4b35bf67f0e5ffa7acc4fd51abe25eb8
chiragnarayanakere <chirag.narayanakere@gmail.com>
2019-12-12 18:13:43 -0800

    Update InnReservations.java

--- InnReservations.java
+++ InnReservations.java
@@ -1,27 +1,40 @@
-import java.sql.*;
-import java.util.*;
-import java.time.LocalDate;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
-
+import java.text.*;
+import java.time.LocalDate;
 
 public class InnReservations {
 
-    String url = "jdbc:mysql://db.labthreesixfive.com/cnarayan?autoReconnect=true&useSSL=false";
-    String name = "cnarayan";
-    String pass = "CSC365-F2019_011277717";
+    //String url = "jdbc:mysql://db.labthreesixfive.com/cnarayan?autoReconnect=true&useSSL=false";
+    //String name = "cnarayan";
+    //String pass = "CSC365-F2019_011277717";
+
+    static String url;
+    static String name;
+    static String pass;
 
     public static void main(String[] args) throws SQLException{
 
         try {
-            InnReservations hp = new InnReservations();
+            InnReservations hp = new InnReservations();            
+            url = args[0];
+            name = args[1];
+            pass = args[2];
 
             while (true) {
             
                hp.display_prompt();
             }
-            
          
         } catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage());
@@ -33,8 +46,6 @@ public class InnReservations {
 
     private void func_req_1() throws SQLException{
    
-      System.out.println("Rooms and Rates");
-
       //create sql statements, pass to function
       String sql1 = "select RoomName, round(sum(DATEDIFF(CheckOut, CheckIn))/180, 2) as popu"
                     + " from cnarayan.lab7_rooms r, cnarayan.lab7_reservations re"
@@ -43,7 +54,18 @@ public class InnReservations {
                     + " group by RoomName"
                     + " order by popu desc";
 
-      String sql2 = "select * from cnarayan.lab7_rooms";
+      String sql2 = "with r1 as (select RoomName, CheckIn, CheckOut"
+                     + " from cnarayan.lab7_rooms r, cnarayan.lab7_reservations re"
+                     + " where r.RoomCode = re.Room),"
+                     + " r2 as (select fir.RoomName as room,"
+                     + " DATEDIFF(sec.CheckIn, fir.CheckOut) as diff,"
+                     + " fir.CheckOut as checkout, sec.CheckIn as checkin," 
+                     + " rank() over (partition by fir.RoomName order by checkout,"
+                     + " DATEDIFF(sec.CheckIn, fir.CheckOut) asc) as RANKING"
+                     + " from r1 as fir join r1 as sec on fir.RoomName = sec.RoomName"
+                     + " where fir.Checkout < sec.CheckIn)"
+                     + " select room, DATE_ADD(checkout, INTERVAL 1 DAY) as Next_Available"
+                     + " from r2 where RANKING = 1 order by room;";
 
       String sql3 = "select RoomName, diff as recent_stay_length, Checkout as recent_checkout from" 
                   + " (select RoomName, DATEDIFF(CheckOut, CheckIn) as diff, Checkout,"
@@ -74,26 +96,60 @@ public class InnReservations {
             String sql3 = s3;
 
             //create new statement per sql query
-            try (Statement stmt = conn.createStatement()) {
+            try (Statement stmt = conn.createStatement();
+                  ResultSet rs = stmt.executeQuery(sql1)) {
 
-               //need to figure out how to output the right values
-                boolean exRes = stmt.execute(sql1);
-                System.out.format("Result: %b %n", exRes);
-                System.out.println("");
+            System.out.println("");
+
+            String r = "RoomName";
+            String pop = "popularity";
+
+            System.out.format("%-30s %-25s", r, pop);
+            System.out.println("");
+
+               while (rs.next()) {
+                    String RoomName = rs.getString("RoomName");
+                    float popu = rs.getFloat("popu");
+                    System.out.format("%-30s%5.2f\n", RoomName, popu);
+                }
+
+               System.out.println("");
+            }
+               
+            try (Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql2)) {
+
+               System.out.format("%-30s%-30s\n", "RoomName", "Next Available Date");
+
+               while (rs.next()) {
+
+                  String room = rs.getString("room");
+                  String date = rs.getString("Next_Available");
+                  System.out.format("%-30s%10s\n", room, date);
+               }
+
+               System.out.println("");
+
             }
 
-            try (Statement stmt2 = conn.createStatement()) {
+            try (Statement stmt = conn.createStatement();
+                  ResultSet rs = stmt.executeQuery(sql3)) {
 
-                boolean exRes2 = stmt2.execute(sql2);
-                System.out.format("Result: %b %n", exRes2);
-                System.out.println("");
-            }
+               String room = "RoomName";
+               String stay = "Stay";
+               String co = "Recent Checkout";
 
-            try (Statement stmt3 = conn.createStatement()) {
+               System.out.format("%-10s %21s %19s", room, stay, co);
+               System.out.println("");
 
-                boolean exRes3 = stmt3.execute(sql3);
-                System.out.format("Result: %b %n", exRes3);
-                System.out.println("");
+               while (rs.next()) {
+                    String RoomName = rs.getString("RoomName");
+                    int rsl = rs.getInt("recent_stay_length");
+                    String date = rs.getString("recent_checkout");
+                    System.out.format("%-30s %-2d %15s\n", RoomName, rsl, date);
+                }
+
+               System.out.println("");
             }
             
         } finally{} 
@@ -396,6 +452,7 @@ public class InnReservations {
     } finally{}
 
    }
+
     // connecting to db for fr3
     private void func_req_3() throws SQLException {
         int code = 0;
@@ -632,10 +689,7 @@ public class InnReservations {
         }
         return valid;
     }
-        
-       
-                 
-
+                   
     private void func_req_4() throws SQLException {
    
       System.out.println("Cancel Reservation");
@@ -673,6 +727,7 @@ public class InnReservations {
             }
     } finally {}
    } 
+
    private void func_req_5() {
    
       System.out.println("Detailed Reservation Information");
@@ -682,12 +737,106 @@ public class InnReservations {
 
    }
 
-   private void func_req_6() {
+   private void func_req_6() throws SQLException{
    
-      System.out.println("Revenue Overview");
+      System.out.println("");
+
+      int[][] rev = new int[10][12];
+      String[] names = new String[10];
 
       //create sql statement, pass to function
-      String sql = "SELECT * FROM lab7_rooms";
+      for (int i = 1; i < 13; i++) {
+
+         fill_table(i, rev);
+      }
+      
+      get_names(names);
+
+      print_table(rev, names);
+
+   }
+
+   private void get_names(String[] names) throws SQLException {
+
+      int count = 0;
+
+      try {
+            Connection conn = DriverManager.getConnection(url, name, pass);
+            
+            String sql = "select RoomName"
+                         + " from cnarayan.lab7_rooms r, cnarayan.lab7_reservations re"
+                         + " where r.RoomCode = re.Room"
+                         + " group by Room"
+                         + " order by Room";
+
+            //create new statement per sql query
+            try (Statement stmt = conn.createStatement();
+                  ResultSet rs = stmt.executeQuery(sql)) {
+
+               //System.out.println("");
+
+               while (rs.next()) {
+                    String RoomName = rs.getString("RoomName");
+                    //int month = rs.int("month");
+                    //String n = rs.getString("Monthly_Revenue");
+                    names[count++] = RoomName;
+                }
+
+              // System.out.println("");
+            }
+
+      } finally {}
+   }
+
+   private void print_table(int[][] rev, String[] names) throws SQLException {
+
+      System.out.format("%-30s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s%-5s\n", "RoomName", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Total");
+
+      for (int i = 0; i < 10; i++) {
+
+         System.out.format("%-30s%-5d%-5d%-5d%-5d%-5d%-5d%-5d%-5d%-5d%-5d%-5d%-5d%-5d", 
+            names[i], rev[i][0], rev[i][1], rev[i][2], rev[i][3], rev[i][4], rev[i][5], rev[i][6],
+            rev[i][7], rev[i][8], rev[i][9], rev[i][10], rev[i][11], 
+            rev[i][0]+rev[i][1]+rev[i][2]+rev[i][3]+rev[i][4]+rev[i][5]+rev[i][6]+rev[i][7]+rev[i][8]+rev[i][9]+ rev[i][10]+rev[i][11]);
+
+         System.out.println("");
+      }
+
+      System.out.println("");
+   }
+
+   private void fill_table(int i, int[][] rev) throws SQLException {
+
+      int roomName_counter = 0;
+
+      try {
+            Connection conn = DriverManager.getConnection(url, name, pass);
+            
+            String sql = "select RoomName, MONTH(CheckOut) as month,"
+                         + " round(SUM((DATEDIFF(CheckOut, CheckIn) * Rate)), 0) as Monthly_Revenue"
+                         + " from cnarayan.lab7_rooms r, cnarayan.lab7_reservations re"
+                         + " where r.RoomCode = re.Room and MONTH(CheckIn) = " + (i)
+                         + " and MONTH(CheckOut) = " + (i) 
+                         + " group by Room, month"
+                         + " order by Room";
+
+            //create new statement per sql query
+            try (Statement stmt = conn.createStatement();
+                  ResultSet rs = stmt.executeQuery(sql)) {
+
+               //System.out.println("");
+
+               while (rs.next()) {
+                    //String RoomName = rs.getString("RoomName");
+                    //int month = rs.int("month");
+                    int mr = rs.getInt("Monthly_Revenue");
+                    rev[roomName_counter++][i-1] = mr;
+                }
+
+              // System.out.println("");
+            }
+
+      } finally {}
 
    }
 
